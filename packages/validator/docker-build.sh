408535e380d570cc77af088ab84c23777fcb251d
Michael Kim <michael.kim.200518@gmail.com>
2025-10-17 17:16:50 +0900

    build(docker): improve image portability and install reliability for validator

--- packages/validator/docker-build.sh
+++ packages/validator/docker-build.sh
@@ -3,4 +3,4 @@
 TAG_NANE="$(git rev-parse --short=12 HEAD)"
 echo "TAG_NANE=$TAG_NANE"
 
-docker build -t bosagora/kios-bridge-validator:"$TAG_NANE" -f Dockerfile --push .
+docker build -t bosagora/kios-bridge-validator:"$TAG_NANE" -f Dockerfile --push ../..
